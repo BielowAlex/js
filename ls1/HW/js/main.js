67235cd2c49dd414b7ad116beb09beb2978edb3f
BielowAlex <97479311+BielowAlex@users.noreply.github.com>
2022-01-31 23:07:48 +0200

    HW

---
+++ ls1/HW/js/main.js
@@ -0,0 +1,116 @@
+// Task1,2
+// let variable = 'hello';
+// console.log(variable);
+// alert(variable);
+// document.write(variable);
+//
+// variable = 'owu';
+// console.log(variable);
+// alert(variable);
+// document.write(" " + variable);
+//
+// variable = 'com';
+// console.log(variable);
+// alert(variable);
+// document.write(" " + variable);
+//
+// variable = 'ua';
+// console.log(variable);
+// alert(variable);
+// document.write(" " + variable);
+//
+// variable = 1;
+// console.log(variable);
+// alert(variable);
+// document.write(" " + variable);
+//
+// variable = 10;
+// console.log(variable);
+// alert(variable);
+// document.write(variable);
+//
+// variable = -999;
+// console.log(variable);
+// alert(variable);
+// document.write(variable);
+//
+// variable = 123;
+// console.log(variable);
+// alert(variable);
+// document.write(variable);
+//
+// variable = 3.14;
+// console.log(variable);
+// alert(variable);
+// document.write(variable);
+//
+// variable = 2.7;
+// console.log(variable);
+// alert(variable);
+// document.write(variable);
+//
+// variable = 16;
+// console.log(variable);
+// alert(variable);
+// document.write(variable);
+//
+// variable = true;
+// console.log(variable);
+// alert(variable);
+// document.write(variable);
+//
+// variable = false;
+// console.log(variable);
+// alert(variable);
+// document.write(variable);
+
+//task3
+let firstName = 'Alex', middleName = 'Nikolajevich', lastName = 'Below';
+let person = [firstName, middleName, lastName];
+
+let i = 0;
+while (i < person.length){
+    console.log(person[i]);
+    i++;
+}
+
+
+//task4
+firstName = prompt("First name");
+console.log(firstName);
+middleName = prompt("Middle name");
+console.log(middleName);
+let years = prompt("How old are you?");
+console.log(years);
+
+alert("Вітаю " + firstName + " " + middleName + ". Тобі " + years + " років.");
+
+
+//task5
+let a = 100, b = '100', c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+//task6
+console.log(5<6);
+console.log(5>6);
+console.log(5>=6);
+console.log(5==6);
+
+console.log(10==10);
+console.log(10===10);
+console.log(10!==10);
+console.log(10!=10);
+console.log(10>10);
+
+console.log(123 === '123');
+console.log(123 == '123');
+
+
+let str = "20";
+ a = 5;
+document.write(str + a + "<br/>");//бо конкатенація відбулась
+document.write(str - a + "<br/>");//бо це ДжаваСкріпт ахахах
+document.write(str * a + "<br/>");//напевне при операторах '*', '/' і '-' сприймають як рівняння
+document.write(str / 2 + "<br/>");//а оператор '+' в такому випадку сприймається як конкатенація і робить з інта вираз
